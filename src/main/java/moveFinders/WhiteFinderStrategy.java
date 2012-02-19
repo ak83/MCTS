6e1aca06848d6fb29ae13920b4fcfb3a7fff2438
Andraz Kohne <andrazkohne@hotmail.com>
2012-02-19 14:11:14 +0100

    done

--- src/main/java/moveFinders/WhiteFinderStrategy.java
+++ src/main/java/moveFinders/WhiteFinderStrategy.java
@@ -1,5 +1,10 @@
 package moveFinders;
 
+/**
+ * Describes white simulation strategies.
+ * 
+ * @author Andraz Kohne
+ */
 public enum WhiteFinderStrategy {
     RANDOM, KRRK_ENDING, KQK_ENDING, KRK_ENDING, KBBK_ENDING
 }
