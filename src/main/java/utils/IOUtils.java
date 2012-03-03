a7e25aec230e7eca24f4109a958afbeef13c29ed
Andraz Kohne <andrazkohne@hotmail.com>
2012-03-03 02:07:33 +0100

    made a method that gets fruits output

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -24,53 +24,63 @@ public class IOUtils {
      *            string to be written in file
      */
     public static void writePGN(String fileName, String input) {
-	try {
+        try {
 
-	    FileWriter fw = new FileWriter(new File(fileName));
-	    fw.write(input);
-	    fw.close();
-	}
-	catch (Exception e) {
-	    e.printStackTrace();
-	}
+            FileWriter fw = new FileWriter(new File(fileName));
+            fw.write(input);
+            fw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static String getOutputFromFruit(String fen) {
+        String rez = null;
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(Constants.FRUIT_FILEPATH);
+            IOUtils.writeToProcess(pr, "ucinewgame");
+            IOUtils.writeToProcess(pr, "setoption name Hash value 128");
+            IOUtils.writeToProcess(pr, "setoption name MultiPV value 100");
+            IOUtils.writeToProcess(pr, "setoption name NalimovPath value "
+                    + Constants.EMD_DIR);
+            IOUtils.writeToProcess(pr, "setoption name NalimovCache value 32");
+            IOUtils.writeToProcess(pr, "setoption name EGBB Path value "
+                    + Constants.EMD_DIR);
+            IOUtils.writeToProcess(pr, "setoption name EGBB Cache value 32");
+            IOUtils.writeToProcess(pr, "position fen " + fen);
+            IOUtils.writeToProcess(pr, "go depth 2");
+            pr.getOutputStream().close();
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr
+                    .getInputStream()));
+
+            String line = null;
+
+            while ((line = input.readLine()) != null) {
+                rez += line + "\n";
+            }
+
+            input.close();
+            pr.destroy();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return rez;
     }
 
 
     public static String getMoveFromFruit(String fen) {
-	String rez = null;
-	try {
-	    Runtime rt = Runtime.getRuntime();
-	    Process pr = rt.exec(Constants.FRUIT_FILEPATH);
-	    IOUtils.writeToProcess(pr, "ucinewgame");
-	    IOUtils.writeToProcess(pr, "setoption name Hash value 128");
-	    IOUtils.writeToProcess(pr, "setoption name MultiPV value 1");
-	    IOUtils.writeToProcess(pr, "setoption name NalimovPath value "
-		    + Constants.EMD_DIR);
-	    IOUtils.writeToProcess(pr, "setoption name NalimovCache value 32");
-	    IOUtils.writeToProcess(pr, "setoption name EGBB Path value "
-		    + Constants.EMD_DIR);
-	    IOUtils.writeToProcess(pr, "setoption name EGBB Cache value 32");
-	    IOUtils.writeToProcess(pr, "position fen " + fen);
-	    IOUtils.writeToProcess(pr, "go depth 2");
-	    pr.getOutputStream().close();
+        String fruitOutput = IOUtils.getOutputFromFruit(fen);
+//        System.out.println(fruitOutput);
+        String[] lines = fruitOutput.split("\n");
 
-	    BufferedReader input = new BufferedReader(new InputStreamReader(
-		    pr.getInputStream()));
+        return lines[lines.length - 1].substring(9, 13);
 
-	    String line = null;
-
-	    while ((line = input.readLine()) != null) {
-		rez = line;
-	    }
-	    input.close();
-	    rez = rez.substring(9);
-	    pr.destroy();
-	}
-	catch (IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
-	return rez;
     }
 
 
@@ -84,17 +94,14 @@ public class IOUtils {
      * @throws IOException
      */
     public static void writeToProcess(Process process, String msg)
-	    throws IOException {
-	char[] chars = (msg + "\n").toCharArray();
-	byte[] bytes = new byte[chars.length];
-	for (int x = 0; x < chars.length; x++) {
-	    bytes[x] = (byte) chars[x];
-	}
-	process.getOutputStream().write(bytes);
+            throws IOException {
+        char[] chars = (msg + "\n").toCharArray();
+        byte[] bytes = new byte[chars.length];
+        for (int x = 0; x < chars.length; x++) {
+            bytes[x] = (byte) chars[x];
+        }
+        process.getOutputStream().write(bytes);
     }
-
-
-    
 
 
     /**
@@ -104,28 +111,29 @@ public class IOUtils {
      *         .
      */
     public static boolean isFruitReady() {
-	String h = null;
-	try {
-	    Runtime rt = Runtime.getRuntime();
-	    Process pr = rt.exec(Constants.FRUIT_FILEPATH);
-	    writeToProcess(pr, "isready");
+        String h = null;
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(Constants.FRUIT_FILEPATH);
+            writeToProcess(pr, "isready");
 
-	    pr.getOutputStream().close();
-	    BufferedReader input = new BufferedReader(new InputStreamReader(
-		    pr.getInputStream()));
+            pr.getOutputStream().close();
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr
+                    .getInputStream()));
 
-	    String line = null;
-	    while ((line = input.readLine()) != null) {
-		h = line;
-	    }
-	    input.close();
-	}
-	catch (Exception e) {
-	    e.printStackTrace();
-	}
-	return h.equals("readyok");
+            String line = null;
+            while ((line = input.readLine()) != null) {
+                h = line;
+            }
+            input.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return h.equals("readyok");
     }
-    
+
+
     private IOUtils() {}
 
 }
